ff54a16a20a65b51a9bdef8354c055aeecd6b71e
Dono <dono.orhan@gmail.com>
2020-06-12 18:38:38 +0200

    feature(pwa): add pwa support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,13 @@
 import './src/styles/global.scss';
 require("prismjs/themes/prism-tomorrow.css");
 require("prismjs/plugins/line-numbers/prism-line-numbers.css");
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+        `This application has been updated. ` +
+        `Reload to display the latest version?`
+    )
+    if (answer === true) {
+        window.location.reload()
+    }
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,68 @@ module.exports = {
                     },
                 ],
             },
-        }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Dorhan`,
+                short_name: `Dorhan`,
+                start_url: `/`,
+                background_color: `#4b81c3`,
+                description: `Freelance web, mobile and video game developer`,
+                lang: `en`,
+                theme_color: `#4b81c3`,
+                display: `standalone`,
+                icon: `static/img/avatar.png`,
+                icons: [
+                    {
+                        src: `favicons/icon-48x48.png`,
+                        sizes: `48x48`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `favicons/icon-72x72.png`,
+                        sizes: `72x72`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `favicons/icon-96x96.png`,
+                        sizes: `96x96`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `favicons/icon-144x144.png`,
+                        sizes: `144x144`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `favicons/icon-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `favicons/icon-256x256.png`,
+                        sizes: `256x256`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `favicons/icon-384x384.png`,
+                        sizes: `384x384`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `favicons/icon-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    }
+                ]
+            }
+        },
+        {
+            resolve: `gatsby-plugin-offline`,
+            options: {
+                precachePages: [`/project/*`, `/blog`, `/posts/*`],
+            },
+        },
     ],
 }
